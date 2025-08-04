11b4087d6e0c655de657848afb7620bdc6c26b6e
unknown <nassirou9993@gmail.com>
2025-08-04 18:23:08 +0200

    fix bug import

--- config/enhanced_fragment_vectorizer.py
+++ config/enhanced_fragment_vectorizer.py
@@ -6,6 +6,7 @@ from gensim.models import Word2Vec
 from collections import Counter, defaultdict
 import hashlib
 from sklearn.preprocessing import StandardScaler
+from scipy.stats import pearsonr
 
 # Set print options to display the entire array
 np.set_printoptions(threshold=np.inf)
